4f841accaa492a4fca4c0b39115605e15c9290ca
lguchen <1355276632@qq.com>
2022-11-14 09:22:05 +0800

    添加注释

--- src/main/java/com/glxt/GlxtApplication.java
+++ src/main/java/com/glxt/GlxtApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-//@MapperScan("com.glxt.mapper")
+//@MapperScan("com.glxt.mapper") //指定接口扫描路径
 @EnableCaching
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)//排除自动注入安全自动配置
 public class GlxtApplication {
 
     public static void main(String[] args) {
